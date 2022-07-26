e65c2572f9627f5b468263e41da6f724beee6d7e
Denis Maina <Denis.maina@student.moringaschool.com>
2022-07-26 20:34:09 +0300

    updated routes

--- my-online-library/src/components/Home.js
+++ my-online-library/src/components/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import BookCard from "./BookCard";
 
 function Home(){
     return(
         <div>
             <p>Hi, welcome to e-library. An online platform where we gain knowledge.</p>
+            <BookCard bookName=" The in Between" bookThumbnail="http://www.pngmart.com/files/3/Toy-Story-Woody-PNG-Photos.png" bookId=" 5464"/>
         </div>
     )
 }
